5b3668b6ff73adb1d448782a141c901a6bb2d5e0
Michael George <georgem@wlwv.k12.or.us>
2019-09-10 08:49:16 -0700

    Added testt class

---
+++ Geometry.java
@@ -0,0 +1,21 @@
+public class Geometry{
+        public static void main(String[] args){
+            int sides = 7;
+            int radius = 5;
+            final double PI = 22/7.0;
+            System.out.println("A Heptagon has " + radius + " sides");
+            System.out.println("A Decagon has " + (sides + 3) + " sides");
+            System.out.println("A Dodecagon has " + (sides + radius) + " sides");
+            System.out.println("The circumference of a circle with radius " + radius + " is " + (2 * PI * radius));
+        }
+    }
+            
+            
+            
+            
+       
+            
+    
+    
+
+    
